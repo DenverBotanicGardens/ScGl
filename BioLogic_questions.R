8495a8cc5a209398eec2e4fe8bb13636fdc8db68
Michelle DePrenger-Levin <deprengm@botanicgardens.org>
2015-08-05 15:26:23 -0600

    Check survival rates of stages and age classes

--- BioLogic_questions.R
+++ BioLogic_questions.R
@@ -5,6 +5,8 @@
 # Your thoughts? I’m thinking the data that would be good for me would be the number of 
 # individuals in Year 1 and their fates over time, with no recruitment data. 
 
+library(ggplot2)
+
 rawscgl <- read.csv("Q:/Research/All_Projects_by_Species/Sclerocactus SPECIES/Sclerocactus_glaucus/R_code_ScGl/R_tables/2014_scgl_rawdata.csv")
 head(rawscgl)
 
@@ -13,6 +15,9 @@ rawscgl[is.na(rawscgl$ScGl_data_id),]
 
 # Add a volume column for individuals
 rawscgl$Vol <- pi*(rawscgl$Width.cm.^2)*rawscgl$Height.cm.
+
+# Add a log volume column
+rawscgl$logVol <- log(rawscgl$Vol)
 
 head(rawscgl)
 
@@ -23,17 +28,180 @@ ggplot(scgl.size, aes(Fl, Vol)) +
   geom_boxplot() +
   facet_grid(.~Site)
 
+# cut off sizes as flowering or not and less than 0 for logVol?
+ggplot(scgl.size, aes(Fl, logVol)) +
+  geom_boxplot() #+
+#  facet_grid(.~Site)
+
 # remove rows with missing data
 names(scgl.size)
 nrow(scgl.size[complete.cases(scgl.size[,c(1:5,10:11)]),])
 nrow(scgl.size)
 
+
 scgl.size <- scgl.size[complete.cases(scgl.size[,c(1:5,10:11)]),]
 
-ggplot(scgl.size, aes(Fl, Vol)) +
+ggplot(scgl.size, aes(Fl, logVol)) +
   geom_boxplot()
 
-ggplot(scgl.size, aes())
+scgl.size$status[scgl.size$Fl == "y"] <- "reproductive"
+scgl.size$status[scgl.size$Fl == 'n'] <- "vegetative"
+scgl.size$status[scgl.size$logVol < 0] <- "mini" 
+scgl.size$status[scgl.size$Height.cm. == 0] <- "dead"
 
-# add stages or size class ...
-scgl.size$stage <- 
+stages <- c("mini","vegetative","reproductive","dead")
+scgl.size$status <- ordered(scgl.size$status, levels = stages)
+
+# average change in size from year to year
+scgl.size <- subset(merge(scgl.size, scgl.size, by = c("Site","Tag")), sort = FALSE, Year.x == Year.y - 1)
+names(scgl.size)
+sg <- scgl.size[,c(1:2,4,10:14,19:21,23,29:33,38:40)]
+names(sg)
+names(sg) <- c("Site","Tag","YRt","HTt","WTHt","FLt","BRt","MNt","VOLt","logVOLt",
+               "stage","YRt1","HTt1",
+               "WTHt1","FLt1","BRt1","MNt1","VOLt1","logVOLt1","fate")
+
+ggplot(sg, aes(HTt,HTt1, colour = Site)) +
+  geom_point() +
+#  facet_wrap(~Site) +
+  geom_smooth(method="lm", se=FALSE)
+
+
+ggplot(sg, aes(VOLt,VOLt1, colour = Site)) +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+
+ggplot(sg, aes(VOLt,VOLt1, colour = stage)) +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+ggplot(subset(sg, !is.infinite(logVOLt) & !is.infinite(logVOLt1)), aes(logVOLt,logVOLt1, colour = Site)) +
+  geom_point() +
+  geom_smooth(method="lm", se=FALSE)
+
+ggplot(sg, aes(x = log(VOLt)))+
+  geom_histogram(binwidth = log(pi), colour = "black", fill = "white")
+  
+
+summary(lm(data=sg, VOLt1~VOLt))
+
+# slope is 0.87 
+
+
+# add stages or size class or age? They grow nearly a cm a year... 
+# a year of growth would be estimated as pi
+yrgr <- pi*(0.87^2)*0.87
+big <- max(sg$VOLt, na.rm = TRUE)
+min(sg$VOLt, na.rm = TRUE)
+big/yrgr
+
+head(sg)
+
+#Check if suvival is related to stage or size, logisitc regression of survival against size
+sg$survive <- 1
+sg$survive[sg$fate == "dead"] <- 0
+
+subset(sg, stage == "dead")
+
+table(sg$stage, sg$survive)
+table(sg$stage)
+sg <- subset(sg, stage != "dead")
+table(sg$stage)
+table(sg$fate)
+table(sg$stage, sg$survive)
+sg$logVOLt[is.infinite(sg$logVOLt)] <- 0
+
+
+summary(glm(survive ~ logVOLt, family = "binomial", data = sg))
+
+for(i in 2009:2013){
+  print(i)
+  print(summary(glm(survive ~ logVOLt, subset = YRt == i, family = "binomial", data = sg)))
+}
+
+ggplot(sg, aes(as.factor(survive), logVOLt)) +
+  geom_boxplot() +
+  facet_wrap(~stage)
+
+#Log-linear model of survival vs. stage
+summary(glm(survive ~ stage, family = poisson, data = sg))
+
+
+
+
+
+#####################################################################
+# Set age ranges assuming they will grow about a cm height and width a year
+# No size data was taken in 2008 so remove
+scgl.size <- subset(rawscgl, Year != 2008)
+
+
+# cut off sizes as flowering or not and less than 0 for logVol?
+ggplot(scgl.size, aes(Fl, logVol)) +
+  geom_boxplot() #+
+#  facet_grid(.~Site)
+
+# remove rows with missing data
+names(scgl.size)
+nrow(scgl.size[complete.cases(scgl.size[,c(1:5,10:11)]),])
+nrow(scgl.size)
+
+
+scgl.size <- scgl.size[complete.cases(scgl.size[,c(1:5,10:11)]),]
+
+ggplot(scgl.size, aes(Fl, logVol)) +
+  geom_boxplot()
+
+ggplot(sg, aes(x = log(VOLt)))+
+  geom_histogram(binwidth = log(pi), colour = "black", fill = "white")
+
+biggest.cut <- log(pi)*6
+bigger.cut <- log(pi)*5
+big.cut <- log(pi)*4
+large.cut <- log(pi)*3
+med.cut <- log(pi)
+small.cut <- log(1)
+
+# Split into more equal parts young, med, bigger, biggest...
+scgl.size$status <- "biggest"
+scgl.size$status[scgl.size$logVol < biggest.cut] <- "bigger"
+scgl.size$status[scgl.size$logVol < bigger.cut] <- "big"
+scgl.size$status[scgl.size$logVol < large.cut] <- "large"
+scgl.size$status[scgl.size$logVol < med.cut] <- "med"
+scgl.size$status[scgl.size$logVol < 0] <- "mini" 
+scgl.size$status[scgl.size$Height.cm. == 0] <- "dead"
+
+stages <- c("mini","med","large","big","bigger","biggest","dead")
+scgl.size$status <- ordered(scgl.size$status, levels = stages)
+
+# average change in size from year to year
+scgl.size <- subset(merge(scgl.size, scgl.size, by = c("Site","Tag")), sort = FALSE, Year.x == Year.y - 1)
+names(scgl.size)
+sg <- scgl.size[,c(1:2,4,10:14,19:21,23,29:33,38:40)]
+names(sg)
+names(sg) <- c("Site","Tag","YRt","HTt","WTHt","FLt","BRt","MNt","VOLt","logVOLt",
+               "stage","YRt1","HTt1",
+               "WTHt1","FLt1","BRt1","MNt1","VOLt1","logVOLt1","fate")
+
+sg$survive <- 1
+sg$survive[sg$fate == "dead"] <- 0
+
+subset(sg, stage == "dead")
+
+table(sg$stage, sg$survive)
+table(sg$stage)
+sg <- subset(sg, stage != "dead")
+table(sg$stage)
+table(sg$fate)
+table(sg$stage, sg$survive)
+sg$logVOLt[is.infinite(sg$logVOLt)] <- 0
+
+#Log-linear model of survival vs. stage
+summary(glm(survive ~ stage, family = poisson, data = sg))
+
+surv.table <- data.frame(table(sg$stage, sg$survive))
+
+# Different sizes do not have significantly different suvival rates
+ggplot(surv.table, aes(as.factor(Var1), Freq, fill = Var2)) +
+  geom_bar(stat="identity")
